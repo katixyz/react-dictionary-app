bce06dc7defe03684db5710e500acc70d9454f10
katixyz <139153535+katixyz@users.noreply.github.com>
2025-03-24 17:19:26 +0100

    Added API documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     let key = "b32c1c35aa482d801df6bo8beet80d48";
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
     let url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(url).then(handleResponse);
   }
