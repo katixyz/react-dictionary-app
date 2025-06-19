1ec1c224f38a9beff68c498e44007a98270909fd
Kati Zech <139153535+katixyz@users.noreply.github.com>
2025-06-19 19:46:13 +0200

    Added a section with pictures and finalized the styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos"
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
+   
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePicsResponse(response) {
+    setPhotos(response.data.photos)
   }
 
   function search(event) {
@@ -17,6 +24,10 @@ export default function Dictionary() {
     // documentation: https://www.shecodes.io/learn/apis/dictionary
     let url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(url).then(handleResponse);
+
+    let picsKey = "b32c1c35aa482d801df6bo8beet80d48";
+    let picsUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${picsKey}`;
+    axios.get(picsUrl).then(handlePicsResponse)
   }
 
   function handleKeywordChange(event) {
@@ -32,6 +43,7 @@ export default function Dictionary() {
       </form>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
